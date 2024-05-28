1d2dc13b3c1fa5bb958609c3b40103442566ac5f
jofebocc <jofebocc@gmail.com>
2024-05-28 01:07:29 -0500

    Source Code for version 1.20.6

---
+++ src/main/java/net/boccalandro/tutorialmod/TurotialModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.boccalandro.tutorialmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TurotialModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
